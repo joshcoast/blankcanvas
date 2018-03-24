a1193a905d5f04820a726c2aaf96b16033057051
Patrick East <patrick.east@icloud.com>
2018-03-24 12:32:43 -0600

    Check for looping Console Errors.

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -13,7 +13,7 @@ firebase.auth().onAuthStateChanged(function (user) {
 	if (user) {
 		// User is signed in.
 	} else {
-		window.location.href="../../../index.html"
+		// window.location.href="../../index.html"
 		// No user is signed in.
 	}
 });
