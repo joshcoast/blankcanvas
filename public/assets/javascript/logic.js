0b09716659e359a363c8950413ffa28a495092a6
Josh Coast <josh@culturefoundry.com>
2018-03-24 13:04:57 -0600

    change the order of the js

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -1,4 +1,4 @@
-// Init Firebase
+// Initialize Firebase
 var config = {
 	apiKey: "AIzaSyCjtkoViUWyLVJYP_NfVaqO8tJoKbvIRj8",
 	authDomain: "blankcanvas-43876.firebaseapp.com",
@@ -8,37 +8,6 @@ var config = {
 	messagingSenderId: "784751662446"
 };
 firebase.initializeApp(config);
-
-firebase.auth().onAuthStateChanged(function (user) {
-	if (user) {
-		// User is signed in.
-	} else {
-		// window.location.href="../../index.html"
-		// No user is signed in.
-	}
-});
-
-
-var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid, emailVerified;
-
-if (user != null) {
-	name = user.displayName;
-	email = user.email;
-	photoUrl = user.photoURL;
-	emailVerified = user.emailVerified;
-	uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-	// this value to authenticate with your backend server, if
-	// you have one. Use User.getToken() instead.
-}
-
-console.log(user);
-console.log(email);
-console.log(uid);
-console.log(photoUrl);
-
-
-
 
 // FirebaseUI config.
 var uiConfig = {
@@ -57,6 +26,33 @@ ui.start('#firebaseui-auth-container', uiConfig);
 
 
 $(document).ready(function () {
+
+	var user = firebase.auth().currentUser;
+	var name, email, photoUrl, uid, emailVerified;
+
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			// User is signed in.
+		} else {
+			// window.location.href="../../index.html"
+			// No user is signed in.
+		}
+	});
+	
+	if (user != null) {
+		name = user.displayName;
+		email = user.email;
+		photoUrl = user.photoURL;
+		emailVerified = user.emailVerified;
+		uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
+		// this value to authenticate with your backend server, if
+		// you have one. Use User.getToken() instead.
+	}
+	console.log("user: " + user);
+	console.log("email: " + email);
+	console.log("uid: " + uid);
+	console.log("photoUrl: " + photoUrl);
+
 	// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 	$('.modal').modal({
 		dismissible: false, // Modal can be dismissed by clicking outside of the modal
