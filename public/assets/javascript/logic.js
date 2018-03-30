d2ee9945163c0adac575183370f4e6b385767666
Patrick East <patrick.east@icloud.com>
2018-03-29 20:08:29 -0600

    User validation working now.

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -28,29 +28,28 @@ $(document).ready(function () {
 	firebase.auth().onAuthStateChanged(function (user) {
 		// database.ref().push({user});
 		if (user) {
+			console.log("Butt");
 			// User is signed in.
 			if (user != null) {
-				console.log("user: " + firebase.auth().currentUser.displayName);
-				console.log("email: " + firebase.auth().currentUser.email);
-				console.log("uid: " + firebase.auth().currentUser.uid);
-				console.log("photoUrl: " + firebase.auth().currentUser.photoUrl);
+				console.log("Test");
 
-				var name = firebase.auth().currentUser.displayName;
-				console.log(currentUser.displayName);
-				var email = firebase.auth().currentUser.email;
-				console.log(currentUser.email);
-				var uid = firebase.auth().currentUser.uid;
-				console.log(currentUser.uid);
-				var photoUrl = firebase.auth().currentUser.photoUrl;
-				console.log(currentUser.photoUrl);
+				var name = user.displayName;
+				console.log(user.displayName);
+				var email = user.email;
+				console.log(user.email);
+				var uid = "/" + user.uid;
+				console.log(user.uid);
+				
 
 				name = user.displayName;
 				email = user.email;
-				photoUrl = user.photoUrl;
 				emailVerified = user.emailVerified;
 				uid = uid; // The user's ID, unique to the Firebase project. Do NOT use
 				// this value to authenticate with your backend server, if
 				// you have one. Use User.getToken() instead.
+
+				database.ref(uid).set({ name: name});
+				$("#idName").text(name);
 			}
 		} else {
 			// window.location.href="../../index.html"
@@ -60,52 +59,7 @@ $(document).ready(function () {
 
 
 
-	// Moved form profile page to here TODO find out what this is
-	$(".dropdown-button").dropdown();
-	$('.slider').slider();
-	$('.collapsible').collapsible();
-	$(".button-collapse").sideNav();
-	// Chips:
-	var chip = {
-		tag: 'chip content',
-		image: '', //optional
-		id: 1, //optional
-	};
-	$('.chips').on('chip.add', function (e, chip) {
-		// you have the added chip here
-	});
-	$('.chips').on('chip.delete', function (e, chip) {
-		// you have the deleted chip here
-	});
-	$('.chips').on('chip.select', function (e, chip) {
-		// you have the selected chip here
-	});
-	$('.chips').material_chip();
-	$('.chips-initial').material_chip({
-		data: [{
-			tag: 'Apple',
-		}, {
-			tag: 'Microsoft',
-		}, {
-			tag: 'Google',
-		}],
-	});
-	$('.chips-placeholder').material_chip({
-		placeholder: 'Enter an interest',
-		secondaryPlaceholder: '+Tag',
-	});
-	$('.chips-autocomplete').material_chip({
-		autocompleteOptions: {
-			data: {
-				'Apple': null,
-				'Microsoft': null,
-				'Google': null
-			},
-			limit: Infinity,
-			minLength: 1
-		}
-	});
-
+	
 	// -- Image Upload -- //
 
 	//Get elements
