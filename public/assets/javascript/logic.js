8e53e6ae6627978dddc166aabd96aef87b126a3f
Patrick East <patrick.east@icloud.com>
2018-03-30 20:56:10 -0600

    Added sing out functionality.

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -15,7 +15,7 @@ var database = firebase.database();
 // Scroll effect for nav bar
 window.addEventListener('scroll', function () {
 	document.body.classList[
-			window.scrollY > 20 ? 'add' : 'remove'
+		window.scrollY > 20 ? 'add' : 'remove'
 	]('scrolled');
 });
 
@@ -27,131 +27,139 @@ $(document).ready(function () {
 
 
 	firebase.auth().onAuthStateChanged(function (user) {
-			// database.ref().push({user});
-			if (user) {
-					// User is signed in.
-					if (user != null) {
+		// database.ref().push({user});
+		if (user) {
+			// User is signed in.
+			if (user != null) {
 
-							var name = user.displayName;
-							console.log(user.displayName);
-							var email = user.email;
-							console.log(user.email);
-							var uid = "/" + user.uid;
-							console.log(user.uid);
+				var name = user.displayName;
+				console.log(user.displayName);
+				var email = user.email;
+				console.log(user.email);
+				var uid = "/" + user.uid;
+				console.log(user.uid);
 
-							var interestChips = [];
-							// This function will Grab the information from the user sign up? Not sure if we will need it.
-							function interestrenderChips() {
-							}
-							// slider and collapsible functions:
-							$('.slider').slider();
-							$('.collapsible').collapsible();
-							$(".button-collapse").sideNav();
-							// Chips:
-							var apiInterest = ""
-							var interest = ""
-							var chip = {
-									tag: 'chip content',
-									image: '', //optional
-									id: 1, //optional
-									Ckey: 'data-key'
-							};
-							// interest chips:
-							$('#ChipsInterest').on('chip.add', function (e, chip) {
-									e.preventDefault();
+				var interestChips = [];
+				// This function will Grab the information from the user sign up? Not sure if we will need it.
+				function interestrenderChips() {
+				}
+				// slider and collapsible functions:
+				$('.slider').slider();
+				$('.collapsible').collapsible();
+				$(".button-collapse").sideNav();
+				// Chips:
+				var apiInterest = ""
+				var interest = ""
+				var chip = {
+					tag: 'chip content',
+					image: '', //optional
+					id: 1, //optional
+					Ckey: 'data-key'
+				};
+				// interest chips:
+				$('#ChipsInterest').on('chip.add', function (e, chip) {
+					e.preventDefault();
 
-									interest = chip.tag;
+					interest = chip.tag;
 
-							});
-							database.ref().on("child_added", function (childSnapshot) {
-									interestChips.push({ interest: childSnapshot.val().interest, key: childSnapshot.key });
-									var chipInit = interestChips.map(chip => ({ tag: chip.interest, key: chip.key }));
-									$('.chips').material_chip({
-											data: chipInit
-									});
-									$('.chip').attr('data-key', childSnapshot.key);
+				});
+				database.ref().on("child_added", function (childSnapshot) {
+					interestChips.push({ interest: childSnapshot.val().interest, key: childSnapshot.key });
+					var chipInit = interestChips.map(chip => ({ tag: chip.interest, key: chip.key }));
+					$('.chips').material_chip({
+						data: chipInit
+					});
+					$('.chip').attr('data-key', childSnapshot.key);
 
-									apiInterest = childSnapshot.val().interest;
-									loadApi()
-							});
+					apiInterest = childSnapshot.val().interest;
+					loadApi()
+				});
 
-							$('.chips').on('chip.delete', function (e, chip) {
-									database.ref('/' + chip.key).remove();
-									$("#InterestDiv").empty();
-									loadApi()
-							});
-							$('.chips').on('chip.select', function (e, chip) {
-									// you have the selected chip here
-							});
-							$('.chips').material_chip();
-							$('.chips-placeholder').material_chip({
-									placeholder: 'Enter an interest',
-									secondaryPlaceholder: '+ interest',
-							});
+				$('.chips').on('chip.delete', function (e, chip) {
+					database.ref('/' + chip.key).remove();
+					$("#InterestDiv").empty();
+					loadApi()
+				});
+				$('.chips').on('chip.select', function (e, chip) {
+					// you have the selected chip here
+				});
+				$('.chips').material_chip();
+				$('.chips-placeholder').material_chip({
+					placeholder: 'Enter an interest',
+					secondaryPlaceholder: '+ interest',
+				});
 
-							name = user.displayName;
-							email = user.email;
-							emailVerified = user.emailVerified;
-							uid = uid; // The user's ID, unique to the Firebase project. Do NOT use
-							// this value to authenticate with your backend server, if
-							// you have one. Use User.getToken() instead.
+				name = user.displayName;
+				email = user.email;
+				emailVerified = user.emailVerified;
+				uid = uid; // The user's ID, unique to the Firebase project. Do NOT use
+				// this value to authenticate with your backend server, if
+				// you have one. Use User.getToken() instead.
 
-							database.ref(uid).set({
-									name: name,
-									email: email,
-									uid: uid,
-				interest: interest,
-									//	profilePic: profilePic
-							});
+				database.ref(uid).set({
+					name: name,
+					email: email,
+					uid: uid,
+					interest: interest,
+					//	profilePic: profilePic
+				});
 
-							$("#idName").text(name);
-							$("#idUrl").text(email);
+				$("#idName").text(name);
+				$("#idUrl").text(email);
 
-							// // -- Image Upload -- //
+				// // -- Image Upload -- //
 
-							// //Get elements
-							// var uploader = $("#uploader");
-							// var fileButton = $("#fileButton");
+				// //Get elements
+				// var uploader = $("#uploader");
+				// var fileButton = $("#fileButton");
 
-							// //Listen for file selection
-							// fileButton.on("change", function (e) {
-							// 	// Get the file
-							// 	var file = e.target.files[0];
-							// 	console.log(e.target.files[0].name);
+				// //Listen for file selection
+				// fileButton.on("change", function (e) {
+				// 	// Get the file
+				// 	var file = e.target.files[0];
+				// 	console.log(e.target.files[0].name);
 
-							// 	// Create a storage ref
-							// 	var storageRef = firebase.storage().ref("profile_pic/" + file.name);
+				// 	// Create a storage ref
+				// 	var storageRef = firebase.storage().ref("profile_pic/" + file.name);
 
-							// 	// Upload file
-							// 	var task = storageRef.put(file);
+				// 	// Upload file
+				// 	var task = storageRef.put(file);
 
-							// 	// Update progress bar 
-							// 	task.on('state_changed',
+				// 	// Update progress bar 
+				// 	task.on('state_changed',
 
-							// 		function error(err) {
+				// 		function error(err) {
 
-							// 		},
-							// 		function complete() {
-							// 			profilePic = task.snapshot.profilePic;
-							// 			console.log("---");
-							// 			console.log(profilePic);
-							// 			console.log("---");
-							// 			$("#profilePicture").attr("src", profilePic);
-							// 			var userPhoto = user + "/" + profilePic;
-							// 			database.ref(userPhoto).set({ profilePic: profilePic });
-							// 		}
-							// 	);
-							// });
-					}
-			} else {
-					window.location.href = "../../index.html"
-					// No user is signed in.
-					console.log("User profile not made.");
+				// 		},
+				// 		function complete() {
+				// 			profilePic = task.snapshot.profilePic;
+				// 			console.log("---");
+				// 			console.log(profilePic);
+				// 			console.log("---");
+				// 			$("#profilePicture").attr("src", profilePic);
+				// 			var userPhoto = user + "/" + profilePic;
+				// 			database.ref(userPhoto).set({ profilePic: profilePic });
+				// 		}
+				// 	);
+				// });
 			}
+		} else {
+			// window.location.href = "../../index.html"
+			// No user is signed in.
+			console.log("User profile not made.");
+		}
 	});
+	$(".signOut").on("click", function () {
+		firebase.auth().signOut().then(function () {
+			console.log("signed out");
+		}).catch(function (error) {
+			console.log("Whoops. An error occured");
+			// An error happened.
+		});
+	})
 
 
-	firebase.auth().currentUser.push({ interest: interest });
+	// firebase.auth().currentUser.push({ interest: interest });
 	//firebase.database().ref().child(firebase.auth().currentUser).push({ interest: interest });
 	//firebase.database().ref().child(uid).push({ interest: interest });
 	//firebase.database().ref().child('uid').push({ interest: interest });
